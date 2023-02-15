bc24b94471634bd703e0683b7199bcef67d45419
ychinata <ychinata@qq.com>
2023-02-15 23:23:40 +0800

    No.: Esp32_Demo_07_adc Author:xy Description:排版

--- esp_32_demo/esp_32_demo_07_adc/e32_demo_adc_recorder/e32_demo_adc_recorder.ino
+++ esp_32_demo/esp_32_demo_07_adc/e32_demo_adc_recorder/e32_demo_adc_recorder.ino
@@ -16,58 +16,54 @@ char partWavData[1024];
 
 
 void setup() {
-  Serial.begin(115200);
-  delay(500);
+	Serial.begin(115200);
+	delay(500);
 
-  // 初始化SD卡
-  if(!sd.begin(SdSpiConfig(5, DEDICATED_SPI, 18000000)))
-  {
-    Serial.println("init sd card error");
-    return;
-  }
+	// 初始化SD卡
+	if(!sd.begin(SdSpiConfig(5, DEDICATED_SPI, 18000000))) {
+		Serial.println("init sd card error");
+		return;
+	}
 
-  //删除并创建文件
-  sd.remove(filename);
-  file = sd.open(filename, O_WRITE|O_CREAT);
-  if(!file)
-  {
-    Serial.println("crate file error");
-    return;
-  }
+	//删除并创建文件
+	sd.remove(filename);
+	file = sd.open(filename, O_WRITE|O_CREAT);
+	if(!file) {
+		Serial.println("crate file error");
+		return;
+	}
 
-  auto header = CreateWaveHeader(1, 44100, 16);
-  header.riffSize = waveDataSize + 44 - 8;
-  header.dataSize = waveDataSize;
-  file.write(&header, 44);
+	auto header = CreateWaveHeader(1, 44100, 16);
+	header.riffSize = waveDataSize + 44 - 8;
+	header.dataSize = waveDataSize;
+	file.write(&header, 44);
 
-  if(!mi.InitAdcInput(I2S_BITS_PER_SAMPLE_16BIT, ADC1_CHANNEL_7))
-  {
-    Serial.println("init i2s error");
-    return;
-  }
+	if(!mi.InitAdcInput(I2S_BITS_PER_SAMPLE_16BIT, ADC1_CHANNEL_7)) {
+		Serial.println("init i2s error");
+		return;
+	}
 
-  Serial.println("start");
+	Serial.println("start");
 
-  for (int j = 0; j < waveDataSize/1024; ++j) {
-    auto sz = mi.Read((char*)communicationData, 2048);
-    char*p =(char*)(communicationData);
-    for(int i=0;i<sz/2;i++)
-    {
-      communicationData[i] = communicationData[i] & 0x0FFF;
-      communicationData[i] *= 7;
-      if(i%2 == 0)
-      {
-          partWavData[i] = p[2*i];
-          partWavData[i + 1] = p[2*i + 1];
-      }
-    }
-    file.write((const byte*)partWavData, 1024);
-  }
-  file.close();
-  Serial.println("finish");
+	for (int j = 0; j < waveDataSize/1024; ++j) {
+		auto sz = mi.Read((char*)communicationData, 2048);
+		char*p =(char*)(communicationData);
+		for(int i=0;i<sz/2;i++) {
+			communicationData[i] = communicationData[i] & 0x0FFF;
+			communicationData[i] *= 7;
+			if(i%2 == 0) {
+				partWavData[i] = p[2*i];
+				partWavData[i + 1] = p[2*i + 1];
+			}
+		}
+		file.write((const byte*)partWavData, 1024);
+	}
+	file.close();
+	Serial.println("finish");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	// put your main code here, to run repeatedly:
 
 }
+
