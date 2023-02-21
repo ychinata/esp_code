784c38134806654cd5b6aa36f8bf682af5de3e08
ychinata <ychinata@qq.com>
2023-02-21 21:37:47 +0800

    No.: Esp32_proj_01_voice_recordplayer Author:xy Description:移植play dac代码

--- esp_32_proj/e32_proj_01_voice_recordplayer/e32_proj_01_voice_recordplayer.ino
+++ esp_32_proj/e32_proj_01_voice_recordplayer/e32_proj_01_voice_recordplayer.ino
@@ -29,16 +29,75 @@
 * IN -- GPIO25/26
 */
 
-
+//record
 MyI2S mi;
 const int record_time = 2;  // second
-//const char filename[] = "/我的录音Adc.wav";
 const char filename[] = "/my_record_adc_2s.wav";
 
 const int waveDataSize = record_time * 88200;
 int16_t communicationData[1024];  //接收缓冲区
 char partWavData[1024];
+//record
 
+//play
+MyI2S mi;
+const char filename[] = "/my_record_adc_2s.wav";
+
+int16_t buffer[1024];        //接收缓冲区, =u8*2048
+int16_t partWavData[2048];   //发往I2S的缓冲区
+//play
+
+//play
+void setup() {
+	Serial.begin(115200);
+	delay(500);
+
+	// 初始化SD卡
+	if(!SD.begin()) {				
+		Serial.println("init sd card error");
+		return;
+	}
+
+	//打印文件	
+    listDir(SD, "/", 0);	
+
+	//打开文件
+    File file = SD.open(filename);	
+	
+	if(!file) {
+		Serial.println("open file error");
+		return;
+	}
+
+	//跳过WAV头部直接到数据部分
+	file.seek(sizeof(WAV_HEADER));
+
+	if(!mi.InitDacOutput(I2S_BITS_PER_SAMPLE_16BIT)) {
+		Serial.println("init i2s error");
+		return;
+	}
+
+	Serial.println("start: read wav file");
+
+	int recvSize = 0;
+	do {
+		recvSize =  file.read((uint8_t*)buffer, 2048);	// u16*1024->u8*2048,代码需要调整
+    	Serial.printf("recvSize: %d\n", recvSize);
+	
+		for(int i = 0; i<recvSize/2; i++) {		//相当于u16*2048
+			partWavData[2*i] = buffer[i];       //左声道,左右声道数据填充一致
+			partWavData[2*i+1] = buffer[i];     //右声道
+		}
+
+		mi.Write((char*)partWavData, recvSize*2);
+	} while(recvSize>0);
+	file.close();
+	mi.End();
+	Serial.println("finish: read wav file");
+}
+
+
+//record
 void setup() {
 	Serial.begin(115200);
 	delay(500);
