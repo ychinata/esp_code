12905e2f71b32274f88d3cb58b9eae53b7c81a57
LenovoXY <ychinata@qq.com>
2024-02-26 11:17:44 +0800

    No.: Esp32_demo_drv8833 Description:first commit 没调通，驱动不了N20电机 (cherry picked from commit 42cf763545edfad88290d7379f8acaaafd63b5e7)
    No.: Esp32_demo_drv8833
    Author:xy
    Description:换了个型号的N20电机就可以驱动了
    (cherry picked from commit c4fa0efa7704a77f33e2f5c6531688fa65c148bd)
    
    No.: Esp32_demo_drv8833
    Author:xy
    Description: 实现四轮前进、后退
    (cherry picked from commit 7330643efb271a8d4af760358005470013f84190)
    
    No.: Esp32_demo_drv8833
    Author:xy
    Description: 实现四轮调速前进、后退。但是，在前进和后退之间，需要stop。待定位。
    (cherry picked from commit 96022f7018f82dbb883b8f1fa9c57fe172f2a661)
    
    No.: Esp32_demo_drv8833
    Author:xy
    Description: 调速时，前后转切换时，估计要先设置方向，再启动PWM。待验证。
    (cherry picked from commit 981f9729a45d43065209160fbebbc9422aa4da90)
    
    No.: Esp32_demo_drv8833
    Author:xy
    Description: 增加原地顺时针旋转CAR4_Clkwise
    (cherry picked from commit a62e2dbff4169ccbe8485077393fbbe0d819b600)
    
    No.: Esp32_demo_drv8833
    Author:xy
    Description: 开源版焊接第二块，花费了一晚上3个钟时间定位电机无法启动现象。
    1.经过逐一排查，最后发现AO1/2都为高电平，BO1/2也为高电平。而正常现象是一高一低，电压差为VM，这样电机才会驱动。
    2.拆除一个电机，发现AO1/2短路，原来是PCB背面电机两极处铺处了两块铜，与N20电机金属外壳，被短路。将电机座拧松一点，即可转动！
    3.另外一个电机拧松也不行，原来是电机线焊接到PCB时，两极之间有一根细铜线飞出来短路了！
    结果非常低级的问题耗了一整晚！！！
    4.后电机的宏BACK_AIN1应该搞错了，这个是前电机的，待排查验证。
    (cherry picked from commit 148bdb32130fed4156f0a85ae312786cb8a97a4b)
    
    No.: Esp32_demo_drv8833
    Author:xy
    Description: 驱动待优化
    (cherry picked from commit e8a3329d47e57f3f44eccb79eec58c4bceaf1111)

---
+++ esp_32_demo/esp_32_demo_08_drv8833/1_drv8833_demo/1_drv8833_demo.ino
@@ -0,0 +1,143 @@
+#include <analogWrite.h>    // 安装库ESP32 AnalogWrite
+#include "car.h"
+/*
+Date:2024.1.10
+*/
+
+void setup() {
+    // put your setup code here, to run once:    
+    Serial.begin(115200);
+    Car_Init();
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    //CAR_Test2();
+    int speed = 255;	// 电机死区待确认30
+    CAR_ForwardAll(speed);  // 前进ok
+}
+
+void CAR_Test2(void) {
+    int speed = 255;    //     
+    CAR4_Clkwise(speed);
+    delay(2000);
+}
+
+// success
+void CAR_Test1(void) {
+    int speed = 255;    // 
+ 
+    CAR_ForwardAll(speed);  // 前进ok
+    delay(2000); 
+    CAR4_ForwardLeft(speed);    // 前左转ok
+    delay(2000); 
+
+    CAR4_StopF();//准备后转
+    CAR4_BackwardLeft(speed);   // 后左转
+    delay(2000);    
+    CAR4_StopF();
+
+    CAR4_StopB();//准备前转
+    CAR4_ForwardRight(speed);   // 前右转ok
+    delay(2000); 
+
+    CAR4_StopF();//准备后转
+    CAR4_BackwardRight(speed);  // 后右转
+    delay(2000);    
+    CAR_BackwardAll(speed); // 单独后退ok
+    delay(2000);
+    CAR4_StopB();//准备前转
+}
+
+//LF-RF-LB-RB
+void CAR_ForwardAll(unsigned int speed) {
+    DRV8833_LF_Forward(speed);
+    DRV8833_RF_Forward(speed);
+    DRV8833_LB_Forward(speed);
+    DRV8833_RB_Forward(speed);
+}
+ 
+void CAR_BackwardAll(unsigned int speed) {
+    DRV8833_LF_Backward(speed);
+    DRV8833_RF_Backward(speed);    
+    DRV8833_LB_Backward(speed);
+    DRV8833_RB_Backward(speed);
+}
+
+
+
+///////////////////////////////DRV8833控制电机
+
+void MOTOR_SetSpeed(int pin, unsigned int speed)
+{
+    if (speed > 255) {
+        speed = 255;    // analogwrite限制
+    }
+    analogWrite(pin, speed);
+}
+
+// FORWARD
+void DRV8833_LF_Forward(unsigned int speed)
+{
+    // LF
+    //digitalWrite(BIN1,1);
+    MOTOR_SetSpeed(BIN1, speed);    
+    digitalWrite(BIN2,0);    
+}
+
+void DRV8833_RF_Forward(unsigned int speed)
+{
+    // RF
+    digitalWrite(AIN1,0);    
+    //digitalWrite(AIN2,1);
+    MOTOR_SetSpeed(AIN2, speed);
+}
+
+void DRV8833_LB_Forward(unsigned int speed)
+{
+    // LB
+    digitalWrite(BACK_AIN1, 0);    
+    digitalWrite(BACK_AIN2,1); 
+    MOTOR_SetSpeed(BACK_AIN2, speed); 
+}
+
+void DRV8833_RB_Forward(unsigned int speed)
+{
+    // RB
+    //digitalWrite(BACK_BIN1,1);
+    MOTOR_SetSpeed(BACK_BIN1, speed);
+    digitalWrite(BACK_BIN2,0);    
+}
+
+// BACKWARD
+void DRV8833_LF_Backward(unsigned int speed)
+{
+    // LF
+    digitalWrite(BIN1,0);
+    //digitalWrite(BIN2,1);
+    MOTOR_SetSpeed(BIN2, speed);
+}
+
+void DRV8833_RF_Backward(unsigned int speed)
+{
+    // RF
+    //digitalWrite(AIN1,1);
+    MOTOR_SetSpeed(AIN1, speed);
+    digitalWrite(AIN2,0);  
+}
+
+void DRV8833_LB_Backward(unsigned int speed)
+{
+    // LB
+    //digitalWrite(BACK_AIN1,1);
+    MOTOR_SetSpeed(BACK_AIN1, speed);
+    digitalWrite(BACK_AIN2,0); 
+}
+
+void DRV8833_RB_Backward(unsigned int speed)
+{
+    // RB
+    digitalWrite(BACK_BIN1,0);
+    //digitalWrite(BACK_BIN2,1);
+    MOTOR_SetSpeed(BACK_BIN2, speed);
+}
---
+++ esp_32_demo/esp_32_demo_08_drv8833/1_drv8833_demo/car.h
@@ -0,0 +1,84 @@
+#ifndef __CAR_H__
+#define __CAR_H__
+
+#include "Arduino.h"
+
+/* 驭风小车V3
+1.U1-AIN25/33,BIN26/27
+U2-AIN16/4,BIN17/18
+*/
+
+// foreward wheel//应该是后轮
+#define PWMA 25
+#define AIN1 25
+#define AIN2 33
+
+#define PWMB 26
+#define BIN1 26
+#define BIN2 27
+
+//backward wheel//应该是前轮
+#define BACK_PWMA 16
+#define BACK_AIN1 16
+#define BACK_AIN2 4
+
+#define BACK_PWMB 17
+#define BACK_BIN1 17
+#define BACK_BIN2 18
+
+
+void motordir(unsigned char out1, unsigned char out2, unsigned char out3, unsigned char out4);
+void motor_Speed(unsigned int PWM1,unsigned int PWM2);
+
+/*******************  本次使用  **********************/
+void motor(int L1,int R1);
+
+void Car_Init();
+void Up(unsigned int pwm);
+void Back(unsigned int pwm);
+void niShiZhen(unsigned int pwm);   //原地逆时针旋转
+void shunShiZhen(unsigned int pwm); //原地顺时针旋转
+void topLeft(unsigned int pwm);
+void topRight(unsigned int pwm);
+void bottomLeft(unsigned int pwm);
+void bottomRight(unsigned int pwm);
+void Stop();
+
+void Upup();  //直线行驶一段距离
+void Left90(); //左转90度
+void Right90(); //右转90度
+void Turn_round(); //掉头
+/****************************************************/
+
+// 2024.2.27 new
+
+#define MOTER_SPEEDDIV  2   // 减速因子
+
+// 单轮控制
+void DRV8833_LF_Forward(unsigned int speed);
+void DRV8833_LB_Forward(unsigned int speed);
+void DRV8833_LF_Backward(unsigned int speed);
+void DRV8833_LB_Backward(unsigned int speed);
+void DRV8833_RF_Forward(unsigned int speed);
+void DRV8833_RB_Forward(unsigned int speed);
+void DRV8833_RF_Backward(unsigned int speed);
+void DRV8833_RB_Backward(unsigned int speed);
+
+// 两轮控制todo
+
+// 四轮控制
+void CAR4_ForwardLeft(unsigned int speed);
+void CAR4_ForwardRight(unsigned int speed);
+void CAR4_BackwardLeft(unsigned int speed);
+void CAR4_BackwardRight(unsigned int speed);
+
+void CAR_ForwardAll(unsigned int speed);
+void CAR_BackwardAll(unsigned int speed);
+
+void CAR4_Stop(void);
+void CAR4_StopF(void);
+void CAR4_StopB(void);
+
+void CAR4_Clkwise(unsigned int speed);
+
+#endif
