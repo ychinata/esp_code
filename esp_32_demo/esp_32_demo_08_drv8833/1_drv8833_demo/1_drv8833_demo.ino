7330643efb271a8d4af760358005470013f84190
ychinata <ychinata@qq.com>
2024-02-27 15:57:38 +0800

    No.: Esp32_demo_drv8833 Author:xy Description: 实现四轮前进、后退

--- esp_32_demo/esp_32_demo_08_drv8833/1_drv8833_demo/1_drv8833_demo.ino
+++ esp_32_demo/esp_32_demo_08_drv8833/1_drv8833_demo/1_drv8833_demo.ino
@@ -13,9 +13,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-    CAR_ForwardM12(0);
-    CAR_ForwardM34(0);
-    delay(100); 
+    CAR_ForwardAll();
+    delay(2000); 
+    CAR_BackwardAll();
+    delay(2000);
 }
 
 void CAR_ForwardM12(unsigned int pwm){  //前进
@@ -38,4 +39,78 @@ void CAR_ForwardM34(unsigned int pwm){  //前进
     digitalWrite(BACK_BIN1,1);
     digitalWrite(BACK_BIN2,0);    
     //motor_Speed(pwm,pwm);
+}
+
+
+//LF-RF-LB-RB
+void CAR_ForwardAll(void) {
+    DRV8833_LF_Forward();
+    DRV8833_RF_Forward();
+    DRV8833_LB_Forward();
+    DRV8833_RB_Forward();
+}
+ 
+void CAR_BackwardAll(void) {
+    DRV8833_LF_Backward();
+    DRV8833_RF_Backward();    
+    DRV8833_LB_Backward();
+    DRV8833_RB_Backward();
+}
+
+// FORWARD
+void DRV8833_LF_Forward(void)
+{
+    // LF
+    digitalWrite(BIN1,1);
+    digitalWrite(BIN2,0);    
+}
+
+void DRV8833_RF_Forward(void)
+{
+    // RF
+    digitalWrite(AIN1,0);    
+    digitalWrite(AIN2,1);  
+}
+
+void DRV8833_LB_Forward(void)
+{
+    // LB
+    digitalWrite(BACK_AIN1,0);    
+    digitalWrite(BACK_AIN2,1); 
+}
+
+void DRV8833_RB_Forward(void)
+{
+    // RB
+    digitalWrite(BACK_BIN1,1);
+    digitalWrite(BACK_BIN2,0);    
+}
+
+// BACKWARD
+void DRV8833_LF_Backward(void)
+{
+    // LF
+    digitalWrite(BIN1,0);
+    digitalWrite(BIN2,1);    
+}
+
+void DRV8833_RF_Backward(void)
+{
+    // RF
+    digitalWrite(AIN1,1);    
+    digitalWrite(AIN2,0);  
+}
+
+void DRV8833_LB_Backward(void)
+{
+    // LB
+    digitalWrite(BACK_AIN1,1);    
+    digitalWrite(BACK_AIN2,0); 
+}
+
+void DRV8833_RB_Backward(void)
+{
+    // RB
+    digitalWrite(BACK_BIN1,0);
+    digitalWrite(BACK_BIN2,1);    
 }
